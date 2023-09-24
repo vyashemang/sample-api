db23ee3cbd88bae5a06cd12cb08b811c1ab42aec
Hemang Vyas <vyashemang563@gmail.com>
2023-09-24 17:00:28 +0530

    initial commit

---
+++ src/test/java/com/sample/sampleapi/SampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.sampleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
